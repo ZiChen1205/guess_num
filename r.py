bae1fc377460b1f0cd89b252b3712519af35fc1b
Chris Huang <happychance1205@gmail.com>
2022-05-26 15:32:15 +0800

    final version

--- r.py
+++ r.py
@@ -1,16 +1,21 @@
 import random
-r = random.randint(1, 100)
-y = 0
+print('這是個猜數字的遊戲,請自行定義最大數字和最小數字')
+x = input('最大數字:')
+y = input('最小數字:')
+x = int(x)
+y = int(y)
+num = random.randint(y, x)
+count = 0
 while True:
-	y += 1
-	x = input('請猜猜看數字是什麼？')
-	x= int(x)
-	if x == r:
+	count += 1
+	guess = input('請猜猜看數字是什麼？')
+	guess = int(guess)
+	if guess == num:
 		print('終於猜對了!!')
-		print('這是你猜的第',y, '次')
+		print('這是你猜的第',count, '次')
 		break
-	elif x > r:
+	elif guess > num:
 		print('比答案大')
-	elif x < r:
+	elif guess < num:
 		print('比答案小')
-	print('這是你猜的第',y, '次')
+	print('這是你猜的第',count, '次')
